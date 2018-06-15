3e940452cfadfc0d577f644b8f631bb31d7e624d
Тодор Макрееев <35991272+tmakreev95@users.noreply.github.com>
2018-06-15 17:57:39 +0300

    Initial Commit

---
+++ javascript/javascript.js
@@ -0,0 +1,22 @@
+/* Scrolls */
+$(".nav-link.team").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".team-container").offset().top
+    }, 2000);
+});
+$(".nav-link.about").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".smart-and-creative-container").offset().top
+    }, 2000);
+});
+$(".nav-link.portfolio").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".portfolio-container").offset().top
+    }, 2000);
+});
+$(".nav-link.pricing").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".pricing-container").offset().top
+    }, 2000);
+});
+/* Scrolls */
